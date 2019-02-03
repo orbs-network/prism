1bdbd847ac4f6a36caa991da985394bfe20afdf0
Gil Amran <gil@orbs.com>
2019-02-03 15:53:33 +0200

    added dummy block hash

--- src/server/fake-blocks-generator.ts
+++ src/server/fake-blocks-generator.ts
@@ -1,10 +1,11 @@
 import { IBlock } from '../shared/IBlock';
+import * as hash from 'object-hash';
 
 let blockHeight = 1;
 export function generateRandomFakeBlock(): IBlock {
   return {
     height: ++blockHeight,
-    hash: '',
+    hash: hash(Math.random()),
     countOfTx: Math.floor(Math.random() * 1000 + 500),
     leanderNode: 'mishmish',
     timestamp: Date.now(),
