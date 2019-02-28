f0256da44412ca740138710ed3efd9c7fcad4c87
Gil Amran <gil@orbs.com>
2019-02-28 18:31:02 +0200

    causing the production work (for now)

--- src/server/orbs-adapter/OrbsAdapterFactory.ts
+++ src/server/orbs-adapter/OrbsAdapterFactory.ts
@@ -3,6 +3,6 @@ import { MockOrbsAdapter } from './MockOrbsAdapter';
 import { OrbsAdapter } from './orbsAdapter';
 
 export function genOrbsAdapter(): IOrbsAdapter {
-  return new OrbsAdapter();
-  // return new MockOrbsAdapter();
+  // return new OrbsAdapter();
+  return new MockOrbsAdapter();
 }
