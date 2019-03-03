b46d9de8929b71ec02d345263b7461f8a3f0175c
itamar Arjuan <itamar.arjuan@gmail.com>
2019-03-03 15:46:41 +0200

    fixing node installation

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get install -yq --no-install-recommends libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 libnss3
-curl -sL https://deb.nodesource.com/setup_11.x | sudo bash -
-sudo apt-get install -y nodejs
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+nvm install 11.2.0
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 node -v
