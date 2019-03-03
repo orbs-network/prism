6567ad08637cabf5b2d688e7f39db7f3b0ca471d
Gil Amran <gil@orbs.com>
2019-03-03 17:58:20 +0200

    back to using the OrbsAdapter

--- src/server/orbs-adapter/OrbsAdapterFactory.ts
+++ src/server/orbs-adapter/OrbsAdapterFactory.ts
@@ -3,6 +3,6 @@ import { MockOrbsAdapter } from './MockOrbsAdapter';
 import { OrbsAdapter } from './OrbsAdapter';
 
 export function genOrbsAdapter(): IOrbsAdapter {
-  // return new OrbsAdapter();
-  return new MockOrbsAdapter();
+  return new OrbsAdapter();
+  // return new MockOrbsAdapter();
 }
