5c4b09139f54dce42dd5d6e07c4557c9290f0cb3
itamar Arjuan <itamar.arjuan@gmail.com>
2019-03-06 15:34:45 +0200

    removing verbosity from Bash

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 sudo apt-get update && sudo apt-get install -yq --no-install-recommends libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 libnss3
 export NVM_DIR="/opt/circleci/.nvm"
@@ -15,8 +15,11 @@ if [ $EXITCODE != 0 ]; then
     exit 1
 fi
 
+echo "Preparing for E2E (Getting gamma and MongoDB)..."
 docker pull orbsnetwork/gamma:experimental
+echo "Running gamma-server on port 9000"
 docker run --name gamma -d -p "9000:8080" orbsnetwork/gamma:experimental
+echo "Running MongoDB server on port 27017"
 docker run --name mongo -d -p "27017:27017" mongo:3.3
 
 npm run build
