7e259adf1321ae69b78fc712f7ff64aacf71fec7
Gil Amran <gil@orbs.com>
2019-03-12 16:40:30 +0200

    Using the gaps detector to know what blocks are missing

--- src/server/gaps-filler/GapsDetector.ts
+++ src/server/gaps-filler/GapsDetector.ts
@@ -3,10 +3,15 @@ import { Storage } from '../storage/storage';
 export async function detectBlockChainGaps(storage: Storage): Promise<Array<bigint>> {
   const result: Array<bigint> = [];
   const latestHeight = await storage.getLatestBlockHeight();
+  console.log(`[Gaps Detector], latestHeight:${latestHeight}`);
   for (let height = 1n; height < latestHeight; height++) {
     const storageBlock = await storage.getBlockByHeight(height.toString());
+    console.log(`[Gaps Detector], do we have block at ${latestHeight}?`);
     if (!storageBlock) {
+      console.log(`[Gaps Detector], nop.`);
       result.push(height);
+    } else {
+      console.log(`[Gaps Detector], yeap.`);
     }
   }
 
--- src/server/gaps-filler/GapsFiller.ts
+++ src/server/gaps-filler/GapsFiller.ts
@@ -1,23 +1,17 @@
 import { OrbsAdapter } from '../orbs-adapter/OrbsAdapter';
 import { Storage } from '../storage/storage';
+import { detectBlockChainGaps } from './GapsDetector';
 
 export class GapsFiller {
   constructor(private storage: Storage, private orbsAdapter: OrbsAdapter) {}
 
   public async fillGaps(): Promise<void> {
-    const latestHeight = await this.storage.getLatestBlockHeight();
-    console.log(`GapsFiller, latestHeight:${latestHeight}`);
-    for (let height = 1n; height < latestHeight; height++) {
-      console.log(`GapsFiller, do we have block at ${latestHeight}?`);
-      const storageBlock = await this.storage.getBlockByHeight(height.toString());
-      if (!storageBlock) {
-        console.log(`GapsFiller, nop. Ask Orbs for this block`);
-        const block = await this.orbsAdapter.getBlockAt(height);
-        await this.storage.handleNewBlock(block);
-        console.log(`GapsFiller, block at ${height} stored`);
-      } else {
-        console.log(`GapsFiller, yeap. Moving on`);
-      }
+    const gaps = await detectBlockChainGaps(this.storage);
+    console.log(`${gaps} missing blocks to fill`);
+    for (const height of gaps) {
+      const block = await this.orbsAdapter.getBlockAt(height);
+      await this.storage.handleNewBlock(block);
+      console.log(`GapsFiller, block at ${height} stored`);
     }
   }
 }
