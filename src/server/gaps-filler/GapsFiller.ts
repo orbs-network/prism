23d18fe8e71c666c91882aaa4ae5765081633579
Gil Amran <gil@orbs.com>
2019-03-13 18:18:27 +0200

    better gaps filler logs

--- src/server/gaps-filler/GapsFiller.ts
+++ src/server/gaps-filler/GapsFiller.ts
@@ -9,7 +9,7 @@ export class GapsFiller {
     const toHeight = await this.storage.getLatestBlockHeight();
     const fromHeight = (await this.storage.getHeighestConsecutiveBlockHeight()) + 1n;
     const gaps = await detectBlockChainGaps(this.storage, fromHeight, toHeight);
-    console.log(`${gaps} missing blocks to fill`);
+    console.log(`${gaps.length} missing blocks to fill`);
     for (const height of gaps) {
       const block = await this.orbsAdapter.getBlockAt(height);
       await this.storage.handleNewBlock(block);
