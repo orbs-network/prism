6d69e340d82c6949290f83c98e1db61d051cd175
Gil Amran <gilamran@gmail.com>
2019-04-10 15:55:08 +0300

    Added rollbar as a logger in winston

--- src/server/logger/LoggerFactory.ts
+++ src/server/logger/LoggerFactory.ts
@@ -1,4 +1,6 @@
 import * as winston from 'winston';
+import { ROLLBAR_ACCESS_TOKEN_SERVER } from '../config';
+import { Rollbar } from 'winston-transport-rollbar';
 
 export function genLogger(toConsole: boolean, toFile: boolean, toRemote: boolean): winston.Logger {
   const logger = winston.createLogger({
@@ -13,6 +15,17 @@ export function genLogger(toConsole: boolean, toFile: boolean, toRemote: boolean
   if (toFile) {
     logger.add(new winston.transports.File({ filename: 'error.log', level: 'error' }));
     logger.add(new winston.transports.File({ filename: 'combined.log' }));
+  }
+
+  if (toRemote && ROLLBAR_ACCESS_TOKEN_SERVER) {
+    const rollbar = new Rollbar({
+      rollbarConfig: {
+        accessToken: ROLLBAR_ACCESS_TOKEN_SERVER,
+        captureUncaught: true,
+        captureUnhandledRejections: true,
+      },
+    });
+    logger.add(rollbar);
   }
 
   return logger;
