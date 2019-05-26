47ceddcf29278aa90b3f18c49cec91b35f6a4a8e
Gil Amran <gil@orbs.com>
2019-05-26 16:39:39 +0300

    Added configuration documentation

--- src/server/config.js
+++ src/server/config.js
@@ -6,53 +6,54 @@
  * The above notice should be included in all copies or substantial portions of the software.
  */
 
+const IS_DEV = process.env.NODE_ENV !== 'production';
+
 const findUp = require('find-up');
-const dotEnvPath = findUp.sync('.env');
-require('dotenv').config({ path: dotEnvPath });
+
+if (IS_DEV) {
+  require('dotenv').config({ path: findUp.sync('.env') });
+}
 
 const { version } = require(findUp.sync('package.json'));
 
 ////////////// CONFIG VARIABLES //////////////
 const PRISM_VERSION = `v${version}`;
 
-// environment
-const IS_DEV = process.env.NODE_ENV !== 'production';
-
 // debug
-const MINIFY_JS_FILES = process.env.MINIFY_JS_FILES === 'true';
-const LOG_TO_CONSOLE = process.env.LOG_TO_CONSOLE === 'true';
-const LOG_TO_FILE = process.env.LOG_TO_FILE === 'true';
-const LOG_TO_ROLLBAR = process.env.LOG_TO_ROLLBAR === 'true';
+const MINIFYED_REACT_JS = process.env.MINIFYED_REACT_JS !== 'false'; // default: true
+const LOG_TO_CONSOLE = process.env.LOG_TO_CONSOLE !== 'false'; // default: true
+const LOG_TO_FILE = process.env.LOG_TO_FILE === 'true'; // default: false
+const LOG_TO_ROLLBAR = process.env.LOG_TO_ROLLBAR === 'true'; // default: false
 
 // server
 const SERVER_PORT = process.env.PORT || 3000;
 const WEBPACK_PORT = 8085; // For dev environment only
-const FORCE_HTTPS = process.env.FORCE_HTTPS === 'true';
+const FORCE_HTTPS = process.env.FORCE_HTTPS !== 'false'; // default: true
 
 // analytics
 const ROLLBAR_ACCESS_TOKEN_SERVER = process.env.ROLLBAR_ACCESS_TOKEN_SERVER;
 
 // database
-const DATABASE_TYPE = process.env.DATABASE_TYPE;
+const DATABASE_TYPE = process.env.DATABASE_TYPE || 'IN_MEMORY';
 const MONGODB_URI = process.env.MONGODB_URI;
-const DB_IS_READ_ONLY = process.env.DB_IS_READ_ONLY === 'true';
+const DB_IS_READ_ONLY = process.env.DB_IS_READ_ONLY === 'true'; // default: false
 
 // orbs client
 const ORBS_ENDPOINT = process.env.ORBS_ENDPOINT;
 const ORBS_VIRTUAL_CHAIN_ID = parseInt(process.env.ORBS_VIRTUAL_CHAIN_ID);
-const ORBS_NETWORK_TYPE = process.env.ORBS_NETWORK_TYPE;
+const ORBS_NETWORK_TYPE = process.env.ORBS_NETWORK_TYPE || 'TEST_NET';
 
 // polling
-const POOLING_INTERVAL = parseInt(process.env.POOLING_INTERVAL);
+const POOLING_INTERVAL = parseInt(process.env.POOLING_INTERVAL || '2000');
 
 // gap filler
-const GAP_FILLER_ACTIVE = process.env.GAP_FILLER_ACTIVE === 'true';
-const GAP_FILLER_INTERVAL = parseInt(process.env.GAP_FILLER_INTERVAL_IN_MINUTES) * 60 * 1000;
+const GAP_FILLER_ACTIVE = process.env.GAP_FILLER_ACTIVE !== 'false'; // default: true
+const GAP_FILLER_INTERVAL = parseInt(process.env.GAP_FILLER_INTERVAL_IN_MINUTES || '30') * 60 * 1000;
 
 module.exports = {
   PRISM_VERSION,
   IS_DEV,
-  MINIFY_JS_FILES,
+  MINIFYED_REACT_JS,
   LOG_TO_CONSOLE,
   LOG_TO_FILE,
   LOG_TO_ROLLBAR,
