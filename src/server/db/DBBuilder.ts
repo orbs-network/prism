3ce25ba806411399559d78a79c22e6f3f7a8d89e
Or Lavy <orlavyil@gmail.com>
2019-12-09 14:01:13 +0200

    Prepares basic structure for DB Builder

--- src/server/db/DBBuilder.ts
+++ src/server/db/DBBuilder.ts
@@ -8,22 +8,39 @@ export class DBBuilder {
 
   public async init(prismVersion: string): Promise<void> {
     const hasBlocks = (await this.db.getLatestBlockHeight()) > 0n;
-    if (!hasBlocks) {
-      await this.rebuildDb();
-    } else {
+
+    if (hasBlocks) {
       const dbVersion = await this.db.getVersion();
+
       if (semver.gt(prismVersion, dbVersion)) {
         await this.db.clearAll();
-        await this.rebuildDb();
+
+        await this.setFreshDbStats(prismVersion);
       }
     }
+
+    await this.rebuildDb();
   }
 
   private async rebuildDb(): Promise<void> {
     const latestHeight = await this.orbsBlocksPolling.getLatestKnownHeight();
+
     for (let h = 1n; h <= latestHeight; h++) {
-      const block = await this.orbsBlocksPolling.getBlockAt(h);
-      await this.storage.handleNewBlock(block);
+      await this.getAndStoreBlock(h);
     }
+  }
+
+  private async getAndStoreBlock(blockHeight: bigint): Promise<void> {
+    const block = await this.orbsBlocksPolling.getBlockAt(blockHeight);
+
+    if (block) {
+      await this.storage.handleNewBlock(block);
+    } else {
+      throw new Error(`ERROR : Got no block response for block ${blockHeight}`);
+    }
+  }
+
+  private async setFreshDbStats(prismVersion: string) {
+    await this.db.setVersion(prismVersion);
   }
 }
