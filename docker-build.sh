ebc82da1c8590b7fdb1be4b991b893a2f45472e5
Gil Amran <gil@orbs.com>
2019-12-09 16:44:49 +0200

    Added missing NVM_DIR to docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash +xe
 
+export NVM_DIR="/opt/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm use 10.17.0
 npm run build
 
