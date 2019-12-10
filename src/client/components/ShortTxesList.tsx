0bd11cb5e48f063de9a612143c205e193e6b0292
Gil Amran <gil@orbs.com>
2019-12-10 15:28:37 +0200

    Removed executionIdx from the view

--- src/client/components/ShortTxesList.tsx
+++ src/client/components/ShortTxesList.tsx
@@ -50,7 +50,6 @@ export const ShortTxesList = withStyles(styles)(({ classes, txes }: IProps) => (
     {txes.length > 0 ? (
       txes.map(tx => (
         <div key={tx.txId}>
-          <ConsoleText className={classes.executionIdx}>{tx.executionIdx}</ConsoleText>
           <ConsoleText className={classes.signerAddress}>{tx.signerAddress}</ConsoleText>
           <ConsoleText id={`tx-${tx.txId.toLowerCase()}`}>
             <PrismLink to={`/tx/${tx.txId}`}>{tx.method}</PrismLink>
