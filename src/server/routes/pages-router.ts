98924a0e083c88bdf6dc4b8e1ef22bf2b3233c40
Gil Amran <gil@orbs.com>
2019-12-19 15:46:47 +0200

    Fixed router for deep links

--- src/server/routes/pages-router.ts
+++ src/server/routes/pages-router.ts
@@ -5,7 +5,7 @@ import { getManifest } from './manifest-manager';
 export function pagesRouter() {
   const router = Router({ mergeParams: true });
 
-  router.get(`/`, async (req, res) => {
+  router.get(`/**`, async (req, res) => {
     const manifest = await getManifest();
     res.render('page.ejs', { manifest, vchainId: config.ORBS_VIRTUAL_CHAIN_ID, prismVersion: `v${config.PRISM_VERSION}` });
   });
