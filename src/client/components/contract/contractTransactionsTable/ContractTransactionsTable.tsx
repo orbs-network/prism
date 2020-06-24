cf8214a888e6df2ddc090a524d5dc50ce5aba7ca
Or Lavy <orlavyil@gmail.com>
2020-06-24 15:43:02 +0300

    Adds all data with styled ux

--- src/client/components/contract/contractTransactionsTable/ContractTransactionsTable.tsx
+++ src/client/components/contract/contractTransactionsTable/ContractTransactionsTable.tsx
@@ -1,18 +1,27 @@
 import React, { useMemo } from 'react';
 import { IContractBlocksInfo } from '../../../../shared/IContractData';
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
+import { makeStyles, Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 import { ConsoleText } from '../../ConsoleText';
 import { PrismLink } from '../../PrismLink';
 import { ShortTxesList } from '../../ShortTxesList';
 import { TransactionsTableBlockRows } from './TransactionsTableBlockRows';
+import { Theme } from '@material-ui/core/styles';
+import red from '@material-ui/core/colors/red';
+import green from '@material-ui/core/colors/green';
 
 interface IProps {
   blocksInfo: IContractBlocksInfo;
   orderedBlockHeights: string[];
   saveToClipboard: (txId: string) => void;
 }
+const useStyles = makeStyles({
+  cellWithCenteredText: {
+    textAlign: 'center',
+  },
+});
 
 export const ContractTransactionsTable = React.memo<IProps>((props) => {
+  const classes = useStyles();
   const { orderedBlockHeights, blocksInfo, saveToClipboard } = props;
 
   const rows = useMemo(() => {
@@ -34,8 +43,12 @@ export const ContractTransactionsTable = React.memo<IProps>((props) => {
     <Table>
       <TableHead>
         <TableRow>
-          <TableCell>Block Height</TableCell>
-          <TableCell>Transactions</TableCell>
+          <TableCell className={classes.cellWithCenteredText}>Block Height</TableCell>
+          <TableCell className={classes.cellWithCenteredText}>Execution index</TableCell>
+          <TableCell className={classes.cellWithCenteredText}>Signer ID</TableCell>
+          <TableCell className={classes.cellWithCenteredText}>Tx ID</TableCell>
+          <TableCell className={classes.cellWithCenteredText}>Method</TableCell>
+          <TableCell className={classes.cellWithCenteredText}>Success</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>{rows}</TableBody>
